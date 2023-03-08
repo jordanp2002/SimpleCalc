4fc401e7924255c414346b995af523162aab7f5e
Jordan <jordanp2002@live.com>
2023-03-07 18:34:58 -0800

    working on functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import "./style.css"
+import {useReducer} from "react";
+const ACTIONS={
+    add_digit: 'add-digit',
+    clear: 'clear',
+    del_digit: 'delete-digit',
+    choose_op: 'choose-op',
+    equal: 'equals'
+}
+function reducer(state,{type, parmeters}){
+    switch(type){
+        case ACTIONS.add_digit:
+            return{
+                ...state,
+                CurrentOP: `${CurrentOP || ""}${parmeters.digit}`
+            }
+    }
+}
 function App() {
+    const [{CurrentOP,previousOP,operation}, dispatch] = useReducer(reducer, {})
   return (
     <div className="calc">
       <div className = "output">
-        <div className="previousOp"></div>
-        <div className ="CurrentOp"></div>
+        <div className="previousOp">{previousOP}{operation}</div>
+        <div className ="CurrentOp">{CurrentOP}</div>
       </div>
-
         <button>/</button>
       <button className="Double">CLEAR</button>
         <button>DEL</button>
@@ -29,5 +46,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;
